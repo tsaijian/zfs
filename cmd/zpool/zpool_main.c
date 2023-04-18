f4b4a4ab7ec060680ad6b7f6cfd8197ba6ca3d3c
Low-power <msl0000023508@gmail.com>
2023-04-19 02:34:41 +0800

    Values printed by zpool-iostat(8) should be right-aligned
    This inappropriate left-alignment was introduced in 7bb7b1f.
    
    Reviewed-by: Tony Hutter <hutter2@llnl.gov>
    Reviewed-by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed-by: Tino Reichardt <milky-zfs@mcmilk.de>
    Signed-off-by: WHR <msl0000023508@gmail.com>
    Closes #14751

--- cmd/zpool/zpool_main.c
+++ cmd/zpool/zpool_main.c
@@ -4261,13 +4261,17 @@ print_iostat_header(iostat_cbdata_t *cb)
  * by order of magnitude. Uses column_size to add padding.
  */
 static void
-print_stat_color(char *statbuf, unsigned int column_size)
+print_stat_color(const char *statbuf, unsigned int column_size)
 {
 	fputs("  ", stdout);
+	size_t len = strlen(statbuf);
+	while (len < column_size) {
+		fputc(' ', stdout);
+		column_size--;
+	}
 	if (*statbuf == '0') {
 		color_start(ANSI_GRAY);
 		fputc('0', stdout);
-		column_size--;
 	} else {
 		for (; *statbuf; statbuf++) {
 			if (*statbuf == 'K') color_start(ANSI_GREEN);
@@ -4282,8 +4286,6 @@ print_stat_color(char *statbuf, unsigned int column_size)
 		}
 	}
 	color_end();
-	for (; column_size > 0; column_size--)
-		fputc(' ', stdout);
 }
 
 /*
